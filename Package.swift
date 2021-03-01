7f4d140b3f7dbd3a8f90c69ad52e2a16d727daa4
Will Townsend <will@townsend.io>
2021-03-01 09:18:17 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftBSV",
     platforms: [
-      .macOS(.v10_12), .iOS(.v8), .tvOS(.v9)
+      .macOS(.v10_12), .iOS(.v9), .tvOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
